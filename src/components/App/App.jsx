a1a1d2c9d9bab4f51cce2f6dfbb95cb706714158
Dmytro <d.salyakin@gmail.com>
2025-06-28 01:46:27 +0300

    Add React Router setup and basic routing structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,17 @@
 import css from './App.module.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../../pages/HomePage/HomePage';
+import RecipeViewPage from '../../pages/RecipeViewPage/RecipeViewPage';
+import NotFound from '../../components/NotFound/NotFound';
 
 export default function App() {
   return (
-    <>
-      <div className={css.container}>
-
-      </div>
-    </>
+    <div className={css.container}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeViewPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
