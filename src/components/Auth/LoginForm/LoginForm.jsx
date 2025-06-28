29a086dc27f9710a098d19a69620add31b2c42a1
Dmytro <d.salyakin@gmail.com>
2025-06-28 03:54:33 +0300

    Add auth structure with placeholder components

---
+++ src/components/Auth/LoginForm/LoginForm.jsx
@@ -0,0 +1,10 @@
+import css from './LoginForm.module.css';
+
+export default function LoginForm() {
+  return (
+    <div className={css.container}>
+      <h2>Login Form</h2>
+      <p>Login form component will be here</p>
+    </div>
+  );
+} 
