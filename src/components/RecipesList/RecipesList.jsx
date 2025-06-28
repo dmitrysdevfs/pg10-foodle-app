4f136b059db003baebe2f9455bbc8ee38fbc25d9
Dmytro <d.salyakin@gmail.com>
2025-06-28 23:31:59 +0300

    Basic implement RecipesList component for rendering recipe array

---
+++ src/components/RecipesList/RecipesList.jsx
@@ -0,0 +1,22 @@
+import css from './RecipesList.module.css';
+
+export default function RecipesList({ recipes }) {
+  console.log('Recipes: ', recipes);
+
+  if (!recipes?.length) {
+    return (
+      <p className={css.message}>There are currently no recipes available.</p>
+    );
+  }
+
+  return (
+    <ul className={css.list}>
+      {recipes.map(recipe => (
+        <li key={recipe.id} className={css.item}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
