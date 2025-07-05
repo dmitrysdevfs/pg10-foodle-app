1bb086524056447057deae2aafa2052ea53fbc7b
Dmytro <d.salyakin@gmail.com>
2025-07-05 16:22:02 +0300

    fix: resolve user name display issue in UserMenu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,23 @@
-import css from './App.module.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { refreshUser } from '../../redux/auth/operations';
 import MainPage from '../../pages/MainPage/MainPage';
 import RecipeViewPage from '../../pages/RecipeViewPage/RecipeViewPage';
 import AuthPage from '../../pages/AuthPage/AuthPage';
 import RecipeAddPages from '../../pages/RecipeAddPage/RecipeAddPage.jsx';
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 import Layout from '../Layout/Layout.jsx';
-
+import css from './App.module.css';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
     <div className={css.container}>
       <Routes>
