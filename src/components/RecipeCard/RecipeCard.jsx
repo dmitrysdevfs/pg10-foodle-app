961cc0e47c20aee4a12b077d10293c38c77a5305
Makson <144148309+Sky65678@users.noreply.github.com>
2025-07-06 16:42:07 +0300

    add savebtn-none-logic recipeCard

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,6 +1,9 @@
 import css from './RecipeCard.module.css';
 import RecipeList from '../RecipeLink/RecipeLink';
 
+import { GoClock } from 'react-icons/go';
+import { CiBookmark } from 'react-icons/ci';
+
 export default function RecipeCard({ recipe, recipes, recipeId }) {
   const { title, description, time, thumb, calories } = recipe;
 
@@ -10,7 +13,10 @@ export default function RecipeCard({ recipe, recipes, recipeId }) {
       <div className={css.titleContainer}>
         <h2 className={css.title}>{title}</h2>
         <div className={css.timeCont}>
-          <button className={css.timeBtn}>{time}</button>
+          <p className={css.timeSvg}>
+            <GoClock size={14.5} />
+          </p>
+          <p className={css.timeTitle}>{time}</p>
         </div>
       </div>
       <div className={css.descriptionContainer}>
@@ -19,7 +25,9 @@ export default function RecipeCard({ recipe, recipes, recipeId }) {
       </div>
       <div className={css.buttonContainer}>
         <RecipeList recipes={recipes} recipeId={recipeId} />
-        <button className={css.savedBtn}>s</button>
+        <button className={css.savedBtn}>
+          <CiBookmark size={24} />
+        </button>
       </div>
     </div>
   );
