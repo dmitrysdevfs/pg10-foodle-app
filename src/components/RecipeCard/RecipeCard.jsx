b4de81c6a6787e91fdeca8c21ebc75aacdc42bc5
Makson <144148309+Sky65678@users.noreply.github.com>
2025-07-08 02:57:39 +0300

    add height title\fix logic image

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -5,7 +5,7 @@ import SaveRecipeButton from '../SaveRecipeButton/SaveRecipeButton.jsx';
 import { GoClock } from 'react-icons/go';
 
 export default function RecipeCard({ recipe, recipes, recipeId, type }) {
-  const { title, description, time, thumb, calories } = recipe;
+  const { title, description, time, thumb, calories, photo } = recipe;
   {
     type === 'favorites' && console.log('proverka: favorites');
   }
@@ -15,16 +15,14 @@ export default function RecipeCard({ recipe, recipes, recipeId, type }) {
 
   return (
     <div className={css.item}>
-      {thumb && (
-        <img
-          src={thumb}
-          alt={title}
-          className={css.image}
-          onError={e => {
-            e.target.style.display = 'none';
-          }}
-        />
-      )}
+      <img
+        src={thumb || photo}
+        alt={title}
+        className={css.image}
+        onError={e => {
+          e.target.style.display = 'none';
+        }}
+      />
       <div className={css.titleContainer}>
         <h2 className={css.title}>{title}</h2>
         <div className={css.timeCont}>
