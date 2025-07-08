df7ede5b4ac95a45c3a21380429770c39b623e84
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-07-08 23:42:58 +0300

    add no_photo in RecepiDetails

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,5 +1,6 @@
 import styles from './RecipeDetails.module.css';
 import SaveRecipeButton from '../SaveRecipeButton/SaveRecipeButton.jsx';
+import NoPhoto from '../../assets/img/no_photo.jpg';
 
 const RecipeDetails = ({ recipe }) => {
 
@@ -8,7 +9,24 @@ const RecipeDetails = ({ recipe }) => {
       <div className={styles.topSection}>
         <h2 className={styles.title}>{recipe.title}</h2>
         <div className={styles.imgContainer}>
-          <img src={recipe.thumb || recipe.photo} alt={recipe.title} className={styles.image} />
+          {/* <img src={recipe.thumb || recipe.photo} alt={recipe.title} className={styles.image} /> */}
+          {recipe.thumb || recipe.photo ? (
+                  <img
+                    src={recipe.thumb || recipe.photo}
+                    alt={recipe.title}
+                    className={styles.image}
+                    onError={e => {
+                      e.target.style.display = 'none';
+                    }}
+                  />
+                ) : (
+                  <img
+                    src={NoPhoto}
+                    alt="Recipe image not available"
+                    className={styles.image}
+                  />
+                )}
+                
         </div>
       </div>
 
